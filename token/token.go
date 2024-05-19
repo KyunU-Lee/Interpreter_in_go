3982ca4dbe772e80099fbee9f3a64ff667522c76
KyunU-Lee <rbsdn3159@gmail.com>
2024-05-19 20:22:04 +0900

    First Test

---
+++ token/token.go
@@ -0,0 +1,32 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 식별자 + 리터럴
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// 연산자
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
