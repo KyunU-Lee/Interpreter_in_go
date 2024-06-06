fef854abafe719ff5c150c52091a15c5bb31eb88
KyunU-Lee <rbsdn3159@gmail.com>
2024-06-06 18:01:46 +0900

    !=, == 테스트 통과

---
+++ repl/repl.go
@@ -0,0 +1,19 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"go/scanner"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for
+	
+}
